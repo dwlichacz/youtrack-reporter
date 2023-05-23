9b8574ccbbcfdeb55c347795b4304fe52ce00a2d
dwlichacz <davidlichacz@gmail.com>
2023-05-23 13:51:58 -0400

    Extract title of issue

--- main.py
+++ main.py
@@ -16,16 +16,22 @@ def call_api_for_issue(issue_id):
 
     full_url = url + f'issues/{issue_id}'
 
+    params = {
+        'fields': 'summary'
+    }
+
     response = requests.get(full_url,
+                            params=params,
                             headers=headers)
 
     if response.status_code == 200:
         retrieved_data = response.json()
-        # Process data
+        title = retrieved_data['summary']
+
     else:
         print(f"Request failed with status code {response.status_code}: {response.text}")
 
 
 if __name__ == '__main__':
-    issue = 'NC-8'
+    issue = 'NC-17'
     call_api_for_issue(issue)
